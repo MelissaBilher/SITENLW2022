f56412fe01e9d4e696c3f26ce617a9b285a516df
Melissa Bilher <mbilherr@gmail.com>
2022-05-09 13:41:26 -0300

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,8 +1,24 @@
+window.addEventListener('scroll', onScroll) // executar a função onScroll ao iniciar a página para não dar erro quando a pagina é aberta e não foi carregado todo o site ainda
+onScroll()
+
 function onScroll() {
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+}
+
+function showNavOnScroll() {
   if (scrollY > 0) {
     navigation.classList.add('scroll')
   } else {
     navigation.classList.remove('scroll')
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 400) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
   }
 }
 
